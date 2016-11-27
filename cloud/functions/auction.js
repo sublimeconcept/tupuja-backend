93ccd59c6807cb75705b43fe82618107931d00c8
cantwait <cadenas.rafael@gmail.com>
2016-11-26 23:01:40 -0500

    #4 first commit

---
+++ cloud/functions/auction.js
@@ -0,0 +1,6 @@
+Parse.cloud.define('updateAuction',function(req,resp){
+	Parse.cloud.useMasterKey();//I don't give a damm about ACL's ';..;'
+	console.log("It Just happened");	
+	console.log("user: ", req.params.user);
+	console.log("auction": req.params.auction);
+});
--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,1 @@
-
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
-});
+require("./functions/auction");
