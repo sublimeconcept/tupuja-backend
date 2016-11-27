f3472d54b69ba1fad4abc4afcf1aafe95fa2d542
cantwait <cadenas.rafael@gmail.com>
2016-11-26 23:06:41 -0500

    #4 fix log function

--- cloud/functions/auction.js
+++ cloud/functions/auction.js
@@ -2,5 +2,5 @@ Parse.cloud.define('updateAuction',function(req,resp){
 	Parse.cloud.useMasterKey();//I don't give a damm about ACL's ';..;'
 	console.log("It Just happened");	
 	console.log("user: ", req.params.user);
-	console.log("auction": req.params.auction);
+	console.log("auction: ", req.params.auction);
 });
