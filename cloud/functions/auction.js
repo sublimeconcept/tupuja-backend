c7b6beba66912b569b6b0416c870db4d8a6e7b4d
cantwait <cadenas.rafael@gmail.com>
2016-11-26 23:22:21 -0500

    #4 adding more logging

--- cloud/functions/auction.js
+++ cloud/functions/auction.js
@@ -1,7 +1,8 @@
 Parse.Cloud.define('updateAuction',function(req,resp){
 	console.log("we are here");
 	Parse.Cloud.useMasterKey();//I don't give a damm about ACL's ';..;'
-	console.log("It Just happened");	
-	console.log("user: ", req.params.user);
-	console.log("auction: ", req.params.auction);
+	console.log("It Just happened");
+	console.log(JSON.stringify(req));	
+	console.log("user: ", req.params.data.user);
+	console.log("auction: ", req.params.data.auction);
 });
